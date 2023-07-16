5912f5a12b68ecb52639f88bbfe039d8a8fac9f0
Facundo Menon Martinez <facundomenon.m@gmail.com>
2023-07-15 22:23:50 -0300

    js agregado

---
+++ js/libros.js
@@ -0,0 +1,27 @@
+let contenedorLibros = document.getElementById("contenedorLibros");
+
+let anchoVentana = window.innerWidth;
+
+
+
+fetch("../../js/productos.json")
+.then((response)=> response.json())
+.then((producto)=>{
+    let libros = producto.filter(libro=> libro.tipo==="libro");
+
+    libros.forEach(item=>{
+        let section = document.createElement("section");
+        section.classList.add("reseñaLibros");
+        section.innerHTML=`
+        <div>
+        <h2>${item.titulo}</h2>
+        <p>${item.descripcion}</p>
+        <button class="boton" id="agregarCarrito${item.id}">Agregar al carrito</button>
+        </div>
+        <img src="${item.img}">`
+
+        contenedorLibros.append(section)
+    })
+    
+})
+
